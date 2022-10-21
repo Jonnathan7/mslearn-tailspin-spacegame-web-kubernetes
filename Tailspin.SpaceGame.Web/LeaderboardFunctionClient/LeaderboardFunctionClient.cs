f7a290ab1580f31e1b6034324e1dbd62c12061aa
Jonnathan <42032897@colegiomedico.org.pe>
2022-10-21 12:21:28 -0500

    Cambiando a HttpClient en vez de WebClient.

--- Tailspin.SpaceGame.Web/LeaderboardFunctionClient/LeaderboardFunctionClient.cs
+++ Tailspin.SpaceGame.Web/LeaderboardFunctionClient/LeaderboardFunctionClient.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace TailSpin.SpaceGame.Web
 {
@@ -18,11 +19,16 @@ namespace TailSpin.SpaceGame.Web
 
         async public Task<LeaderboardResponse> GetLeaderboard(int page, int pageSize, string mode, string region)
         {
+            Task<string> task = new HttpClient().GetStringAsync(new Uri($"{this._functionUrl}?page={page}&pageSize={pageSize}&mode={mode}&region={region}"));
+            string json = await task;
+             
+             return JsonSerializer.Deserialize<LeaderboardResponse>(json);
+            /*
             using (WebClient webClient = new WebClient())
             {
                 string json = await webClient.DownloadStringTaskAsync($"{this._functionUrl}?page={page}&pageSize={pageSize}&mode={mode}&region={region}");
                 return JsonSerializer.Deserialize<LeaderboardResponse>(json);
-            }
+            }*/
         }
     }
 }
